37de17fd699452859f4afa6b12a8485c900182aa
“syyyym” <gabi.lupusor@gmail.com>
2019-11-28 00:41:02 +0000

    add hashpage and message page, render dynamically with ejs

--- routes/uiroute.js
+++ routes/uiroute.js
@@ -1,14 +1,15 @@
 const crypto = require('crypto');
 const express = require('express');
+const path = require('path');
 const router = express.Router();
 
-// Index page as in domain.com/
+router.use(express.static('public'));
+
 router.get('/', (req,res)=>{
 	//messages page is the home page
 	res.redirect('/ui/messages');
 }).post('/', (req,res)=>{ res.status(404).end('404'); });
 
-router.use(express.static('public'));
 // Create form and db save function
 router.get('/messages', (req,res)=>{
 	res.sendFile( process.cwd() + '/views/index.html' );
@@ -22,44 +23,43 @@ router.post('/messages', (req,res)=>{
 		if (r.length==0) {
 			ms.insertOne({message:message,hash:hash},(err,rep)=>{
 				if (err) { console.log(0); return false; }
-				//res.send(/*'Not saved before ' +*/ hash);
-				res.send('<html><head><title>Index</title></head><body>' +
-					'<b>SHA256 Hash of your string:</b><br><br>' + hash +
-					'</body></body></html>');
+				res.render('hashpage', {
+					hash: hash,
+					message: message
+				});
 			});
 		} else {
-			//res.send(/*'Already found ' +*/ hash);
-			res.send('<html><head><title>Index</title></head><body>' +
-				'<b>SHA256 Hash of your string:</b><br><br>' + hash +
-				'</body></body></html>');
-
+			res.render('hashpage', {
+				hash: hash,
+				message: message
+			});
 		}
 	});
 });
 
 // Get the GET hash and spit out the original msg or else show 404 error
-router.get('/messages/:hash([a-fA-F0-9]+)', (req,res)=>{
-	res.set({'Content-Type': 'application/json'});
-	let hash = req.params.hash;
+router.post('/hashing', (req,res, next)=>{	
+	let hash = req.body.hash;
+	res.set({'Content-Type': 'text/html'});
 	// Find and get msg. 
 	db.collection('messages')
 	.find({hash:hash},{projection:{_id:0,hash:0}}).limit(1)
 	.toArray((e,r)=>{
 		if (!e) {
 			if (r.length) {
-				res.send(JSON.stringify(r[0],null,' '));
+				let message = r[0].message;
+				res.render('messagepage', {
+					hash: hash,
+					message: r[0].message
+				});				
 			} else if (r.length==0) {
 				const errmsg = 'Message not found';
-				res.status(404).send(JSON.stringify({err_msg:errmsg},null,' '));
+				res.render('errorpage', {
+					errmsg: errmsg
+				});	
 			}
 		} else {res.send('["connection_error"]')}
 	});
 });
-
-router.post('/messages/hashing', (req,res)=>{	
-	let hash = req.body.hash;
-	res.redirect('/ui/messages/' + hash );
-})
-
 
 module.exports = router;
